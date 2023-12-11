4fce5906755aedcadbe51310e7480250f66bf042
MartinGbz <martin.grabarz@outlook.fr>
2023-12-12 00:43:16 +0100

    fix: month display

--- src/components/dashboard/token-stats/token-stats.tsx
+++ src/components/dashboard/token-stats/token-stats.tsx
@@ -11,7 +11,8 @@ interface TokenStatsProps {
 const getShortnedDate = (timestampInSecond: bigint) => {
   const date = new Date(Number(timestampInSecond * BigInt(1000)));
   const day = date.getDate() < 10 ? "0" + date.getDate() : date.getDate();
-  const month = date.getMonth() < 10 ? "0" + date.getMonth() : date.getMonth();
+  const month =
+    date.getMonth() + 1 < 10 ? "0" + date.getMonth() + 1 : date.getMonth() + 1;
   return day + "/" + month + "/" + date.getFullYear();
 };
 
